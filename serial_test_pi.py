2c5a3b0299c1289902101efcd4cf00bc54f39259
wallfacer1018 <110882728+wallfacer1018@users.noreply.github.com>
2024-06-02 01:03:47 +0800

    serial test for raspberry pi

---
+++ serial_test_pi.py
@@ -0,0 +1,22 @@
+import serial
+
+# 设置串口参数
+serial_port = '/dev/ttyS0'  # 根据实际情况设置
+baud_rate = 9600            # 波特率
+
+# 打开串口
+ser = serial.Serial(serial_port, baud_rate)
+
+try:
+    while True:
+        if ser.in_waiting > 0:
+            # 读取一行数据
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+
+except KeyboardInterrupt:
+    print("程序结束")
+
+finally:
+    # 关闭串口
+    ser.close()
